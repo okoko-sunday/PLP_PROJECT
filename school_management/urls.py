5d3872fcef3af74e16b7cc0c5543ac23571f9db5
okoko-sunday <okoko.sunday17@gmail.com>
2024-11-30 03:27:30 +0100

    added a background image

--- school_management/urls.py
+++ school_management/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.home, name='home' ),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('student/', views.student, name='student'),
     path('student<str:class_name>/', views.student_filter, name='student_filter'),
     path('teacher/', views.teacher, name='teacher'),
+    path('admin/', admin.site.urls, name="admin")
 ]
